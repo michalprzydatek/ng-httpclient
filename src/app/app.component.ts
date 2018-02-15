6934de475ea81406cf6960d88d4744092447c950
Michal Przydatek <michalprzydatek@gmail.com>
2018-02-15 20:00:19 +0100

    ooo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app';
 
+  constructor(private httpService: HttpService) {
+
+  }
+
+  getPosts() {
+    this.httpService.getPosts().subscribe( posts => {
+      console.log(posts);
+    });
+  }
+
+  getPost() {
+
+  }
+
+  addPost() {
+    const post: Post = ({
+      userId: 1,
+      id: null,
+      title: 'Mój post',
+      body: 'body'
+    });
+  }
+
+  changePost() {
+    const p: Post = ({
+      id: 1,
+      body: 'zmieniam tylko wpis',
+    });
+  }
 
 }
 
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Post } from './app.component';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
 
 @Injectable()
 export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  getPosts(){
-    this.http.get('https://jsonplaceholder.typicode.com/posts');
+  getPosts(): Observable<Array<Post>> {
+    return this.http.get<Array<Post>>('https://jsonplaceholder.typicode.com/posts');
   }
 
 }
